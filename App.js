f2cfc1ba9c820bba6ac2919dbc050d7264595afd
naka-muraa <akirayuto0718@gmail.com>
2021-08-25 09:59:07 +0900

    ポップアップ画面

--- App.js
+++ App.js
@@ -1,19 +1,82 @@
 import * as React from 'react';
-import { SafeAreaView ,StyleSheet ,Button, View, Text ,TextInput, } from 'react-native';
+import { SafeAreaView ,StyleSheet ,Button, View, Text ,TextInput, Modal, TouchableOpacity, Animated, Dimensions} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Table, Row, Rows,TableWrapper, Col  } from 'react-native-table-component';
-import d_1 from './教育.json'; 
-import d_2 from './教育学.json'; 
-import d_3 from './教育学 (教職).json'; 
-import d_4 from './教養教育.json'; 
-import d_5 from './自然科学.json'; 
-import d_6 from './人間科学.json'; 
-import d_7 from './人間社会科学.json'; 
-import d_8 from './生物資源.json'; 
-import d_9 from './総合理工 (博士後期) .json'; 
-import d_10 from './法文.json'; 
+import d_1 from './assets/firstSemisterLecs/教育.json'; 
+import d_2 from './assets/firstSemisterLecs/教育学.json'; 
+import d_3 from './assets/firstSemisterLecs/教育学（教職）.json'; 
+import d_4 from './assets/firstSemisterLecs/教養教育.json'; 
+import d_5 from './assets/firstSemisterLecs/自然科学.json'; 
+import d_6 from './assets/firstSemisterLecs/人間科学.json'; 
+import d_7 from './assets/firstSemisterLecs/人間社会科学.json'; 
+import d_8 from './assets/firstSemisterLecs/生物資源.json'; 
+import d_9 from './assets/firstSemisterLecs/総合理工.json'; 
+import d_10 from './assets/firstSemisterLecs/総合理工（博士後期）.json';
+import d_11 from './assets/firstSemisterLecs/法文.json';
 
+
+//JSONからデータをまとめる関数
+const makeClassdeta= ({}) => {
+
+   const class_1 = JSON.parse(d_1);
+   const class_2 = JSON.parse(d_2);
+   const class_3 = JSON.parse(d_3);
+   const class_4 = JSON.parse(d_4);
+   const class_5 = JSON.parse(d_5);
+   const class_6 = JSON.parse(d_6);
+   const class_7 = JSON.parse(d_7);
+   const class_8 = JSON.parse(d_8);
+   const class_9 = JSON.parse(d_9);
+   const class_10 = JSON.parse(d_10);
+   const class_11 = JSON.parse(d_11);
+
+   
+
+}
+
+
+//画面サイズ取得 ←なんかできない？？
+var {HEIGHT, WIDTH} = Dimensions.get('window');
+console.log(HEIGHT,WIDTH);
+
+//ポップアップ画面
+const Modalpoup = ({visible, children}) => {
+
+  const [showmodal, setshowmodal] = React.useState(visible);
+  const scalevalue = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    togglemodal();
+  }, [visible]);
+  const togglemodal = () =>{
+    if(visible){
+      setshowmodal(true);
+      Animated.spring(scalevalue,{
+        toValue:1,
+        duration:500,
+        useNativeDriver: true,
+      }).start();
+    }else{
+      setTimeout(() => setshowmodal(false),300); //モデルが閉じる前にアニメーションを実行
+      Animated.spring(scalevalue, {
+        tovalue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return ( 
+    <Modal transparent visible={showmodal}>
+      <View style={styles.modalbacground}>
+        <Animated.View 
+          style={[styles.modalcontainer, {transform:[{scale: scalevalue}]}]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
 
 //時間割管理ホーム画面
 function HomeScreen({ navigation }) {
@@ -27,6 +90,8 @@ function HomeScreen({ navigation }) {
     ['1', '2', '3', '4','5'],
     ['1', '2', '3', '4','5'],
   ];
+
+  const [visible,setVisible] = React.useState(false);
 
   return (
     <SafeAreaView style={styles.container} >
@@ -51,6 +116,24 @@ function HomeScreen({ navigation }) {
       </View>
 
       <View style={styles.bottom}>
+        <View>
+          <Modalpoup visible = {visible}>
+            <View style={{alignItems:'center'}}>
+              <View style={styles.modalheader}>
+
+                <TouchableOpacity>
+                  <Button title="入力完了" onPress={() => setVisible(false)}/>
+                </TouchableOpacity>
+
+                <Text　style={{fontSize:20, marginVertical:20, fontWeight:'bold'}}>所属する学部を入力してください</Text>      
+                <TextInput style={styles.popuptext} placeholder="例　生物資源科学部"></TextInput>
+
+              </View>
+            </View>
+          </Modalpoup>
+          <Button title="ポップアップ" onPress={() => setVisible(true)}/>
+        </View>
+
         <View style={styles.buttoncontainer}>
           <Button title="編集" onPress={() => navigation.navigate('')}/>
         </View>
@@ -161,12 +244,13 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    //backgroundColor: 'powderblue',
-    backgroundColor:'#fff'
+    //height:HEIGHT,
+    backgroundColor:'#fff',
   },
   upper: {
     width: "100%",
     flex:1,
+    //height: HEIGHT * 0.2,
     backgroundColor: "#fff",
     flexDirection:'row',
     justifyContent:'space-around',
@@ -186,6 +270,8 @@ const styles = StyleSheet.create({
     alignItems:'flex-end',
   },
   input: {
+    color:'black',
+    fontSize:20,
     width: 210,
     borderColor:'black',
     borderWidth:2,
@@ -232,6 +318,34 @@ const styles = StyleSheet.create({
     alignItems:'center',
     marginVertical:25,
     flex:1,
+  },
+  modalbacground: {
+    flex:1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  modalcontainer: {
+    width:'80%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation:20,
+  },
+  modalheader: {
+    width:"100%",
+    alignItems:'center',
+    justifyContent:"center",
+
+  },
+  popuptext: {
+    backgroundColor:'#d7e0ff',
+    width:"80%",
+    paddingVertical:10,
+    borderWidth:2,
+    fontSize:25,
+    color:'black',
   },
 });
 
