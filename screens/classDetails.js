79fb36c2a2078e367757b9184698fd6f10b5eb59
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-09-22 16:02:27 +0900

    update search screen and inplement function to pass data to homeScreen

--- screens/classDetails.js
+++ screens/classDetails.js
@@ -4,12 +4,12 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 //授業詳細画面
 export default function classDetails({ navigation, lectureInfo }) {
-  const { 科目 } = lectureInfo.params
-  const { 担当 } = lectureInfo.params
-  const { 教室名 } = lectureInfo.params
+  const { 科目 } = lectureInfo.params;
+  const { 担当 } = lectureInfo.params;
+  const { 教室名 } = lectureInfo.params;
   let balnkClass = null;
   if (教室名 == '') {
-    balnkClass = 'データがありません';
+    balnkClass = '未定またはオンライン講義です';
   } else {
     balnkClass = 教室名;
   }
@@ -22,7 +22,7 @@ export default function classDetails({ navigation, lectureInfo }) {
         <Text style={styles.classTapText}>{科目}</Text>
       </View>
       <View style={styles.classTapframe}>
-        <Text style={styles.classTapHeader}>担当者名</Text>
+        <Text style={styles.classTapHeader}>担当者</Text>
         <Text style={styles.classTapText}>{担当}</Text>
       </View>
       <View style={styles.classTapframe}>
@@ -35,51 +35,60 @@ export default function classDetails({ navigation, lectureInfo }) {
       </View>
       <View style={styles.ctTuikaContainer}>
         <TouchableOpacity style={styles.ctTuikaBtn} onPress={() => navigation.goBack()}>
-          <Text style={styles.ctTuikaBtnText}>追加</Text>
+          <Text style={styles.ctTuikaBtnText}>戻る</Text>
         </TouchableOpacity>
       </View>
       </View>
-    </ScrollView>
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  // 全体のコンテナ
   containerClass: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "#ffffff",
   },
+  // 各情報について
   classTapframe: {
-    marginVertical: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#78bbc7',
   },
   classTapHeader: {
-    fontSize: 30,
-    color: 'blue',
-    fontWeight: 'bold',
+    width: '100%',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    fontSize: 24,
+    color: 'white',
+    fontWeight: '600',
   },
   classTapText: {
-    fontSize: 30,
+    padding: 5,
+    textAlign: 'center',
+    width: '100%',
+    fontSize: 22,
+    backgroundColor: '#e6f2f5',
     color: 'black',
-    fontWeight: 'bold',
-    marginTop: 20,
   },
+  // 追加ボタン関連
   ctTuikaContainer: {
     width: '30%',
     alignItems: 'center',
+    marginTop: 30,
   },
   ctTuikaBtn: {
-    borderWidth: 2,
+    borderWidth: 1,
     borderRadius: 10,
-    borderColor: 'black',
-    backgroundColor: '#ffd2bc'
+    borderColor: '#dcdcdc',
+    backgroundColor: '#78bbc7'
   },
   ctTuikaBtnText: {
-    fontSize: 30,
-    padding: 10,
+    paddingRight: 30,
+    paddingLeft: 30,
+    paddingVertical: 5,
+    fontSize: 20,
     fontWeight: 'bold',
-    color: 'black',
+    color: 'white',
   }
 });
