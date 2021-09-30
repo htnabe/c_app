4bc0313342e7cfc8385f4aebb34c1101a38ff62d
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-09-30 23:49:25 +0900

    revise design on bottom lectures and add building's name

--- screens/homeLectureDetail.js
+++ screens/homeLectureDetail.js
@@ -3,22 +3,45 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useRoute, useNavigation } from '@react-navigation/native';
 
+// 教室名と棟名が空白の場合の処理
+function changeName(rName, bName,) {
+  if (rName == '' || bName == '') {
+    return '未定またはオンライン講義です';
+  }
+  else if (bName != '' && rName == '') {
+    return bName;
+  }
+  else if (bName == '' && rName != '') {
+    return rName;
+  }
+  else if (bName != '' && rName != '') {
+    return bName + '\n' + rName;
+  }
+};
+
 //授業詳細画面
 export default function lectureDetail() {
   const navigation = useNavigation();
   const route = useRoute();
   const lectureName = route.params.科目;
   const teacher = route.params.担当;
-  let roomName = route.params.教室名;
   const dayTime = route.params.曜日時限;
-  if (roomName == '') {
-    roomName = '未定またはオンライン講義です';
+  let roomName = route.params.教室名;
+  let buildingName = route.params.棟名;
+  let buildName = route.params.棟;
+  let displayedRoomName = '';
+
+  if (buildingName == undefined && buildName != undefined) {
+    displayedRoomName = changeName(roomName, buildName);
   }
+  else if (buildingName != undefined && buildName == undefined) {
+    displayedRoomName = changeName(roomName, buildingName);
+  }
+
   const classDayTime = dayTime.split(',')
   const daytime_1 = classDayTime[0] + 'コマ';
   const daytime_2 = classDayTime[1] + 'コマ';
   const showDaytime = daytime_1 + '・' + daytime_2;
-
 
   return (
     <ScrollView>
@@ -36,8 +59,10 @@ export default function lectureDetail() {
           <Text style={styles.classTapText}>{showDaytime}</Text>
         </View>
         <View style={styles.classTapframe}>
-          <Text style={styles.classTapHeader}>教室名</Text>
-          <Text style={styles.classTapText}>{roomName}</Text>
+          <Text style={styles.classTapHeader}>棟名・教室名</Text>
+          <View style={styles.buildingRoom}>
+          <Text style={styles.classTapText}>{displayedRoomName}</Text>
+          </View>
         </View>
         <View style={styles.ctTuikaContainer}>
           <TouchableOpacity style={styles.ctTuikaBtn} onPress={() => navigation.goBack()}>
@@ -76,6 +101,9 @@ const styles = StyleSheet.create({
     fontSize: 22,
     backgroundColor: '#e6f2f5',
     color: 'black',
+  },
+  buildingRoom: {
+    marginTop: '2%'
   },
   // 追加ボタン関連
   ctTuikaContainer: {
